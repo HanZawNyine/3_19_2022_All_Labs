0773b2af8e0fe41c332759d49e6dfe81270f283e
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-22 18:42:45 +0630

    live_camera no permission

---
+++ kivy/flask/flasky/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+
+if __name__ == "__main__":
+    app.run(Debug=True)
