c43c4d0d58f27a58c6b762a4a17f1bf4f3625a94
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-23 10:07:57 +0630

    update repo

--- camera_no_need_permission/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import home_api_view
-
-urlpatterns = [
-    path('',home_api_view),
-]
--- django_live_camera/camera_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Home,gg
-
-urlpatterns = [
-    path('gg/', Home),
-    path('',gg)
-]
